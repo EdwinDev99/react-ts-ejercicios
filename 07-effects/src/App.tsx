621d0e482cdcc064cd521815e019a2cbdf2ad866
EdwinDev99 <edwindev99@gmail.com>
2025-08-08 15:22:48 +0100

    useEffect with fetc and .then

--- 07-effects/src/App.tsx
+++ 07-effects/src/App.tsx
@@ -1,5 +1,23 @@
+import useUser from "./hooks/useUser";
+
 function App() {
-  return <div>Hola mundo</div>;
+  const { loading, error, users } = useUser();
+
+  if (loading) {
+    return <p>Cargando...</p>;
+  }
+
+  if (error && !loading) {
+    return <p>Ha ocurrido un error{error}</p>;
+  }
+
+  return (
+    <ul>
+      {users.map((u) => (
+        <li key={u.id}>{u.name}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;

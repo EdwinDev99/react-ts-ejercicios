848c6245d34296cc4a60bc4b7898d8a8239ecccc
EdwinDev99 <edwindev99@gmail.com>
2025-08-25 14:38:53 +0100

    exercise of counting clicks on the website title

--- 08-click-counter/src/App.tsx
+++ 08-click-counter/src/App.tsx
@@ -1,5 +1,20 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <p>hola mundo</p>;
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Clicks: ${count}`;
+    console.log(document.title);
+  }, [count]);
+
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count -- {count}
+      </button>
+    </div>
+  );
 }
 
 export default App;

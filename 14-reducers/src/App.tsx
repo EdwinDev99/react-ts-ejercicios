b70b3ef927102734554bfef1392a1d353c09e91d
EdwinDev99 <edwindev99@gmail.com>
2025-09-03 13:57:02 +0100

    exercise with useState

--- 14-reducers/src/App.tsx
+++ 14-reducers/src/App.tsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+
 function App() {
-  return <div>Hola</div>;
+  const [contador, setContador] = useState(0);
+
+  const sumar = () => {
+    setContador(contador + 1);
+  };
+
+  const restar = () => {
+    setContador(contador - 1);
+  };
+
+  const reset = () => {
+    setContador(0);
+  };
+
+  return (
+    <>
+      <h1>Hola mundo{contador}</h1>
+      <button onClick={sumar}>Incrementar</button>
+      <button onClick={restar}>Reducir</button>
+      <button onClick={reset}>Reset</button>
+    </>
+  );
 }
 
 export default App;

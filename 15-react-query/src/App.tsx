2c317e1fb32489784412af095f487d15af05536b
EdwinDev99 <edwindev99@gmail.com>
2025-09-09 10:57:00 +0100

    Using Query with loading status and errors

--- 15-react-query/src/App.tsx
+++ 15-react-query/src/App.tsx
@@ -1,7 +1,36 @@
 import "./App.css";
+import { useQuery } from "@tanstack/react-query";
 
-function App() {
-  return <div>Hola mundo</div>;
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+};
+
+const queryTodos = (): Promise<Todo[]> =>
+  fetch("https://jsonplaceholder.typicode.com/todos").then((response) => {
+    if (!response.ok) throw Error(`Error${response.status}`);
+    return response.json();
+  });
+
+export default function App() {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["todos"],
+    queryFn: queryTodos,
+  });
+
+  if (error) return <h2>{error.message}:(</h2>;
+  if (isLoading) return <h2>Cargando.....</h2>;
+
+  return (
+    <>
+      <h2>Todos</h2>
+      <ul>
+        {data?.map((todo) => (
+          <li>{todo.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
-
-export default App;
